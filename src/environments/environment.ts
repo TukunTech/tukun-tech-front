ea42ccbfa3cc6743772c39ce8876d00d31582ff7
oscaredu1226 <oscaredu01@hotmail.com>
2025-09-17 11:56:48 -0500

    build(config): add path aliases and base environments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
 
-  apiUrl: 'http://localhost:8085/api/v1',
+  apiUrl: 'http://localhost:8080/api/v1',
   wsUrl: 'ws://localhost:8080/api/v1',
 
   appName: 'TUKUNTECH',
