98aa938f6def377d697831e2b48c1b6c37a334c4
AdriPCH <adriana03pch@gmail.com>
2025-09-19 21:08:33 -0500

    feat(update): update permitions

--- src/app/features/subscription/pages/subscription.ts
+++ src/app/features/subscription/pages/subscription.ts
@@ -1,10 +1,27 @@
-import {Component} from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {AuthFacade} from '@feature/auth/application/auth.facade';
+import {NgIf} from '@angular/common';
+import {
+  PatientPersonalInfoComponent
+} from '@feature/patient/components/patient/personal-info/patient-personal-info.component';
+import {PatientSubscription} from '@feature/subscription/components/patient/patient-subscription/patient-subscription';
+type AppRole = 'PATIENT' | 'ATTENDANT' | 'ADMINISTRATOR';
 
 @Component({
   selector: 'app-subscription',
-  imports: [],
+  imports: [
+    NgIf,
+    PatientPersonalInfoComponent,
+    PatientSubscription
+  ],
   templateUrl: './subscription.html',
 })
 export class SubscriptionComponent {
+  private auth = inject(AuthFacade);
 
+  private readonly roles: AppRole[] = (this.auth.user()?.roles ?? []) as AppRole[];
+
+  readonly isPatient: boolean = this.roles.includes('PATIENT');
+  readonly isAttendant: boolean = this.roles.includes('ATTENDANT');
+  readonly isAdmin: boolean = this.roles.includes('ADMINISTRATOR');
 }
