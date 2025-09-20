c7ea18c0798f88fc23dd42cc6d8d242f33d432fa
AdriPCH <adriana03pch@gmail.com>
2025-09-19 22:39:43 -0500

    feature(support-permissions)

--- src/app/features/support/pages/support.ts
+++ src/app/features/support/pages/support.ts
@@ -1,10 +1,25 @@
-import {Component} from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {AuthFacade} from '@feature/auth/application/auth.facade';
+import {NgIf} from '@angular/common';
+import {PatientSubscription} from '@feature/subscription/components/patient/patient-subscription/patient-subscription';
+import {SupportPatient} from '@feature/support/components/patient/support-patient/support-patient';
+type AppRole = 'PATIENT' | 'ATTENDANT' | 'ADMINISTRATOR';
 
 @Component({
   selector: 'app-support',
-  imports:[],
+  imports: [
+    NgIf,
+    PatientSubscription,
+    SupportPatient
+  ],
   templateUrl: '/support.html',
 })
 export class SupportComponent {
+  private auth = inject(AuthFacade);
 
+  private readonly roles: AppRole[] = (this.auth.user()?.roles ?? []) as AppRole[];
+
+  readonly isPatient: boolean = this.roles.includes('PATIENT');
+  readonly isAttendant: boolean = this.roles.includes('ATTENDANT');
+  readonly isAdmin: boolean = this.roles.includes('ADMINISTRATOR');
 }
