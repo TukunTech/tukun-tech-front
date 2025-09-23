be12fbb50e0baebb9da8ecf55db213f7508f5c08
AdriPCH <adriana03pch@gmail.com>
2025-09-23 11:16:35 -0500

    update(history): update role in history.ts

--- src/app/features/history/pages/history.ts
+++ src/app/features/history/pages/history.ts
@@ -1,10 +1,19 @@
-import {Component} from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {AuthFacade} from '@feature/auth/application/auth.facade';
+
+
+type AppRole = 'PATIENT' | 'ATTENDANT' | 'ADMINISTRATOR';
 
 @Component({
   selector: 'app-history',
   imports: [],
   templateUrl: '/history.html'
 })
-export class HistoryComponent {
 
+export class HistoryComponent {
+  private auth = inject(AuthFacade);
+  private readonly roles: AppRole[] = (this.auth.user()?.roles ?? []) as AppRole[];
+  readonly isPatient: boolean = this.roles.includes('PATIENT');
+  readonly isAttendant: boolean = this.roles.includes('ATTENDANT');
+  readonly isAdmin: boolean = this.roles.includes('ADMINISTRATOR');
 }
