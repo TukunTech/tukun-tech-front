b38e433be3b4121fa12070a82b2fd681ee07aa5b
AdriPCH <adriana03pch@gmail.com>
2025-09-23 11:17:20 -0500

    feat(history): create history component

---
+++ src/app/features/history/components/patient/patient-history.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'patient-history',
+  imports: [],
+  templateUrl: 'patient-history.html',
+  styleUrls: ['patient-history.css'],
+})
+
+export class PatientHistoryComponent {
+
+}
