26b895d863a350c7f064a4703820ac4160dd0e2f
oscaredu1226 <oscaredu01@hotmail.com>
2025-09-23 15:51:42 -0500

    feat(auth/ui): add register form component

--- src/app/features/auth/ui/components/register-form/register-form.component.ts
+++ src/app/features/auth/ui/components/register-form/register-form.component.ts
@@ -1,35 +1,66 @@
 import {Component, inject} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {FormBuilder, ReactiveFormsModule, Validators} from '@angular/forms';
+import {Registration, Role} from '@feature/auth/domain/entities/registration';
+import {AuthFacade} from '@feature/auth/application/auth.facade';
+import {Router, RouterLink} from '@angular/router';
+import {ToastService} from '@core/ui/toast/toast.service';
+import {ToastContainerComponent} from '@core/ui/toast/components/toast-container.component';
 
 @Component({
   standalone: true,
   selector: 'app-register-form',
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, ToastContainerComponent],
   templateUrl: './register-form.component.html',
   styleUrls: ['./register-form.component.css'],
 })
 export class RegisterFormComponent {
   private fb = inject(FormBuilder);
+  private facade = inject(AuthFacade);
+  private router = inject(Router);
+  private toast = inject(ToastService);
+
+  roles: Role[] = ['PATIENT', 'ATTENDANT'];
 
   form = this.fb.group({
     name: ['', Validators.required],
     lastName: ['', Validators.required],
     dni: ['', Validators.required],
-    age: [null, [Validators.required, Validators.min(0)]],
-    sex: ['', Validators.required],
-    bloodGroup: ['', Validators.required],
-    nationality: ['', Validators.required],
+    age: [null],
+    sex: [''],
+    bloodGroup: [''],
+    nationality: [''],
     allergy: [''],
     email: ['', [Validators.required, Validators.email]],
     password: ['', [Validators.required, Validators.minLength(6)]],
+    role: ['', Validators.required],
   });
 
-  submit() {
+
+  async submit() {
     if (this.form.invalid) {
       this.form.markAllAsTouched();
+      this.toast.warning('Data is missing or some fields are invalid');
       return;
     }
-    console.log('register payload', this.form.value);
+
+    try {
+      const reg = Registration.fromForm(this.form.value);
+
+      const s = await this.facade.register(reg);
+
+      this.toast.success('Registration created successfully');
+
+      if (s.accessToken) {
+        const home = this.facade.getHomeByRole();
+        await this.router.navigateByUrl(home);
+      } else {
+        await this.router.navigateByUrl('/auth/login');
+      }
+    } catch (e: any) {
+      console.error(e);
+      this.toast.error(e?.message || 'We were unable to complete the registration');
+    }
   }
+
 }
