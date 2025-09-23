dd3fc47d4968c336a9d6d734d1cbfc259e3c7fc6
oscaredu1226 <oscaredu01@hotmail.com>
2025-09-23 15:52:30 -0500

    chore(support): add support pages

--- src/app/features/support/pages/support.ts
+++ src/app/features/support/pages/support.ts
@@ -1,7 +1,6 @@
 import {Component, inject} from '@angular/core';
 import {AuthFacade} from '@feature/auth/application/auth.facade';
 import {NgIf} from '@angular/common';
-import {PatientSubscription} from '@feature/subscription/components/patient/patient-subscription/patient-subscription';
 import {SupportPatient} from '@feature/support/components/patient/support-patient/support-patient';
 type AppRole = 'PATIENT' | 'ATTENDANT' | 'ADMINISTRATOR';
 
@@ -9,7 +8,6 @@ type AppRole = 'PATIENT' | 'ATTENDANT' | 'ADMINISTRATOR';
   selector: 'app-support',
   imports: [
     NgIf,
-    PatientSubscription,
     SupportPatient
   ],
   templateUrl: '/support.html',
