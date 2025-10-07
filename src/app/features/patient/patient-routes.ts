0d7c30541ae1f7de172e8f7ffcdcfb4fe1396f2b
oscaredu1226 <oscaredu01@hotmail.com>
2025-10-07 17:47:59 -0500

    feat(patient): add route for patient update view and connect it to PatientUpdateComponent

--- src/app/features/patient/patient-routes.ts
+++ src/app/features/patient/patient-routes.ts
@@ -1,12 +1,17 @@
 import {Route} from '@angular/router';
 import {authGuard} from '@core/guards/auth.guard';
 
-
 export const patientRoutes: Route[] = [
   {
     path: '',
     canMatch: [authGuard],
     loadComponent: () =>
       import('./pages/patient').then(m => m.PatientComponent),
+  },
+  {
+    path: 'update',
+    canMatch: [authGuard],
+    loadComponent: () =>
+      import('./components/patient/patient-info-update/patient-personal-info-update.component').then(m => m.PatientPersonalInfoUpdateComponent),
   }
 ]
