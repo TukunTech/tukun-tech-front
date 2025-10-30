4c05190af74df16402892f22b2e2e1c49030ea6d
oscaredu1226 <oscaredu01@hotmail.com>
2025-10-30 18:22:33 -0500

    refactor(routes): update app routing configuration and onboarding navigation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import {activeAccountGuard} from '@core/guards/active-account.guard';
 import {MainLayoutComponent} from '@layout/main-layout/main-layout.component';
 import {redirectToRoleHomeGuard} from '@core/guards/redirect-to-role-home.guard';
 import {RedirectToRoleComponent} from '@core/components/redirect-to-role/edirect-to-role.component';
+import {profileExistsGuard} from '@core/guards/profile-exists.guard';
 
 export const routes: Routes = [
   {
@@ -11,63 +12,102 @@ export const routes: Routes = [
     component: MainLayoutComponent,
     canActivate: [authGuard, activeAccountGuard],
     children: [
-      { path: '', pathMatch: 'full', redirectTo: 'dashboard' },
+      {path: '', pathMatch: 'full', redirectTo: 'dashboard'},
 
-      { path: 'dashboard', component: RedirectToRoleComponent, canActivate: [redirectToRoleHomeGuard] },
-
+      {
+        path: 'dashboard',
+        component: RedirectToRoleComponent,
+        canActivate: [redirectToRoleHomeGuard],
+      },
       {
         path: 'dashboard/admin',
         loadChildren: () =>
-          import('@feature/dashboard/admin/routes/admin.routes').then(m => m.adminDashboardRoutes),
+          import('@feature/dashboard/admin/routes/admin.routes').then(
+            (m) => m.adminDashboardRoutes
+          ),
       },
       {
         path: 'dashboard/attendant',
         loadChildren: () =>
-          import('@feature/dashboard/attendant/routes/attendant.routes').then(m => m.attendantDashboardRoutes),
+          import('@feature/dashboard/attendant/routes/attendant.routes').then(
+            (m) => m.attendantDashboardRoutes
+          ),
       },
       {
         path: 'dashboard/patient',
+        canActivate: [profileExistsGuard],
         loadChildren: () =>
-          import('@feature/dashboard/patient/routes/patient.routes').then(m => m.patientDashboardRoutes),
+          import('@feature/dashboard/patient/routes/patient.routes').then(
+            (m) => m.patientDashboardRoutes
+          ),
       },
+
+
       {
         path: 'patient',
-        loadChildren: () => import('@feature/patient/patient-routes').then(
-          (m) => m.patientRoutes,
-        ),
+        loadChildren: () =>
+          import('@feature/patient/patient-routes').then((m) => m.patientRoutes),
       },
       {
         path: 'subscription',
-        loadChildren: () => import('@feature/subscription/subscription-routes').then(
-          (m) => m.subscriptionRoutes,
-        )
+        loadChildren: () =>
+          import('@feature/subscription/subscription-routes').then(
+            (m) => m.subscriptionRoutes
+          ),
       },
       {
         path: 'support',
-        loadChildren: () => import('@feature/support/support-routes').then(
-          (m) => m.supportRoutes,
-        )
+        loadChildren: () =>
+          import('@feature/support/support-routes').then(
+            (m) => m.supportRoutes
+          ),
       },
       {
         path: 'history',
-        loadChildren: () =>import('@feature/history/history-routes').then(
-          (m) => m.historyRoutes,
-        )
+        loadChildren: () =>
+          import('@feature/history/history-routes').then(
+            (m) => m.historyRoutes
+          ),
       },
       {
         path: 'vital-signs',
-        loadChildren: () => import('@feature/vital-signs/vital-signs-routes').then(
-          (m) => m.vitalSignsRoutes,
-        )
-      }
+        loadChildren: () =>
+          import('@feature/vital-signs/vital-signs-routes').then(
+            (m) => m.vitalSignsRoutes
+          ),
+      },
     ],
   },
+
   {
     path: 'auth',
     loadChildren: () =>
-      import('@feature/auth/routes/auth.routes').then(m => m.authRoutes),
+      import('@feature/auth/routes/auth.routes').then((m) => m.authRoutes),
   },
-  { path: '403', loadComponent: () => import('@shared/pages/forbidden/forbidden.component').then(m => m.ForbiddenPage) },
-  { path: '404', loadComponent: () => import('@shared/pages/not-found/not-found.component').then(m => m.NotFoundPage) },
-  { path: '**', redirectTo: 'dashboard' },
+
+  {
+    path: '403',
+    loadComponent: () =>
+      import('@shared/pages/forbidden/forbidden.component').then(
+        (m) => m.ForbiddenPage
+      ),
+  },
+
+  {
+    path: '404',
+    loadComponent: () =>
+      import('@shared/pages/not-found/not-found.component').then(
+        (m) => m.NotFoundPage
+      ),
+  },
+
+  {
+    path: 'onboarding',
+    loadComponent: () =>
+      import('@feature/onboarding/component/profile-onboarding.component').then(
+        (m) => m.ProfileOnboardingComponent
+      ),
+  },
+
+  {path: '**', redirectTo: 'dashboard'},
 ];
