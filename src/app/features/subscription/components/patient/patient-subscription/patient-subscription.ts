6f2bcee905edab92ebc85bec894d48fd0148b994
oscaredu1226 <oscaredu01@hotmail.com>
2025-10-30 18:23:05 -0500

    refactor(subscription): align patient subscription logic with new onboarding flow

--- src/app/features/subscription/components/patient/patient-subscription/patient-subscription.ts
+++ src/app/features/subscription/components/patient/patient-subscription/patient-subscription.ts
@@ -7,7 +7,6 @@ import { Component } from '@angular/core';
   styleUrl: './patient-subscription.css'
 })
 export class PatientSubscription {
-  // Estos valores luego se reemplazan con el backend
   daysLeft = 10;
   currentPrice = 20;
   planName = 'Individual plan';
