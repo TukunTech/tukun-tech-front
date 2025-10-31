5a9dd18c4e492afc74832b78b618c2612dce0ea9
oscaredu1226 <oscaredu01@hotmail.com>
2025-10-31 09:54:01 -0500

    refactor(patient): remove mock data and connect component to store

--- src/app/features/patient/pages/patient.ts
+++ src/app/features/patient/pages/patient.ts
@@ -1,23 +1,21 @@
 import {Component, inject} from '@angular/core';
+import {NgIf} from '@angular/common';
 import {AuthFacade} from '@feature/auth/application/auth.facade';
 import {
-  PatientPersonalInfo,
   PatientPersonalInfoComponent
 } from '@feature/patient/components/patient/personal-info/patient-personal-info.component';
-import {NgIf} from '@angular/common';
+import {PatientStore, PatientPersonalInfo} from '@feature/patient/data/patient.store';
 
 type AppRole = 'PATIENT' | 'ATTENDANT' | 'ADMINISTRATOR';
 
 @Component({
   selector: 'app-patient',
-  imports: [
-    PatientPersonalInfoComponent,
-    NgIf
-  ],
+  imports: [PatientPersonalInfoComponent, NgIf],
   templateUrl: './patient.html',
 })
 export class PatientComponent {
-  private auth = inject(AuthFacade);
+  private readonly auth = inject(AuthFacade);
+  private readonly patientStore = inject(PatientStore);
 
   private readonly roles: AppRole[] = (this.auth.user()?.roles ?? []) as AppRole[];
 
@@ -25,14 +23,7 @@ export class PatientComponent {
   readonly isAttendant: boolean = this.roles.includes('ATTENDANT');
   readonly isAdmin: boolean = this.roles.includes('ADMINISTRATOR');
 
-  readonly info: PatientPersonalInfo = {
-    name: 'Rocco',
-    lastName: 'Rodríguez',
-    dni: '74854932',
-    age: 32,
-    sex: 'Male',
-    bloodGroup: 'A+',
-    nationality: 'Brazilian',
-    allergy: 'Penicillin'
-  };
+  get info(): PatientPersonalInfo | null {
+    return this.patientStore.patient();
+  }
 }
